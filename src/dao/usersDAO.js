564879678b635223a2a4c8dc1cc2d7ba05f74c00
kokou kpadenou <kokou2kpadenou@gmail>
2021-03-30 17:48:29 -0400

    durable writes and user preferences

--- src/dao/usersDAO.js
+++ src/dao/usersDAO.js
@@ -60,11 +60,14 @@ export default class UsersDAO {
       // Insert a user with the "name", "email", and "password" fields.
       // TODO Ticket: Durable Writes
       // Use a more durable Write Concern for this operation.
-      await users.insertOne({
-        name: userInfo.name,
-        email: userInfo.email,
-        password: userInfo.password,
-      })
+      await users.insertOne(
+        {
+          name: userInfo.name,
+          email: userInfo.email,
+          password: userInfo.password,
+        },
+        { w: "majority" },
+      )
       return { success: true }
     } catch (e) {
       if (String(e).startsWith("MongoError: E11000 duplicate key error")) {
@@ -174,8 +177,8 @@ export default class UsersDAO {
       // TODO Ticket: User Preferences
       // Use the data in "preferences" to update the user's preferences.
       const updateResponse = await users.updateOne(
-        { someField: someValue },
-        { $set: { someOtherField: someOtherValue } },
+        { email: email },
+        { $set: { preferences: preferences } },
       )
 
       if (updateResponse.matchedCount === 0) {
